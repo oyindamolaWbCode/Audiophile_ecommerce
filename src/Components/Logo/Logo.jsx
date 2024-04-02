50ae0ba9fddad3516f516df088cf53e30abeaafb
oyindamolaWbCode <kushimo.oyindamola@gmail.com>
2024-04-02 11:34:56 +0100

    header

---
+++ src/Components/Logo/Logo.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import logo from "../../assets/logo.svg";
+import "./logo.css";
+
+function Logo(props) {
+  return <img src={logo} alt="logo" className="logo" />;
+}
+
+export default Logo;
--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import Logo from "../Logo/Logo";
+import cart from "../../assets/icon-cart.svg";
+import "./navigation.css";
+import { NavLink } from "react-router-dom";
 
 function Navigation(props) {
-  return <div>navigate</div>;
+  return (
+    <>
+      <nav className="nav">
+        <Logo />
+        <div className="nav__nav-links collapsed">
+          <NavLink className="nav__nav-link" to="/">
+            Home
+          </NavLink>
+          <NavLink className="nav__nav-link" to="headphones">
+            Headphones
+          </NavLink>
+          <NavLink className="nav__nav-link" to="/speakers">
+            Speakers
+          </NavLink>
+          <NavLink className="nav__nav-link" to="/earphones">
+            Earphones
+          </NavLink>
+        </div>
+        <img src={cart} alt="nav-cart" className="cart" />
+      </nav>
+    </>
+  );
 }
 
 export default Navigation;
