bbf3183410bec8e275f9e8ea582cc2aa79a41daa
oyindamolaWbCode <kushimo.oyindamola@gmail.com>
2024-07-17 11:35:26 +0100

    upperpage

--- src/Components/Earphones/Earphones.jsx
+++ src/Components/Earphones/Earphones.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import NavHeader from "../NavHeaders/NavHeader";
-
+import Upper from "../Uppercomponent/Upper";
+import earphoneImg from "../../assets/Images/image-earphones.png";
+import "./Earphones.css";
 const Earphones = () => {
   return (
     <div className="earphone_container">
       <NavHeader centerIt="Earphones" />
+      <Upper
+        UpperImg={earphoneImg}
+        UpperTitle="new product"
+        UpperSubTitle="YX1 WIRELESS EARPHONES"
+        UpperParagraph="Tailor your listening experience with bespoke dynamic drivers from the new YX1 Wireless Earphones. Enjoy incredible high-fidelity sound even in noisy environments with its active noise cancellation feature."
+      />
     </div>
   );
 };
--- src/Components/Speaker/Speakers.jsx
+++ src/Components/Speaker/Speakers.jsx
@@ -2,10 +2,17 @@ import React from "react";
 import "./Speaker.css";
 import NavHeader from "../NavHeaders/NavHeader";
 import Upper from "../Uppercomponent/Upper";
+import speakerImg from "../../assets/Images/image-speaker-zx9 (1).png";
 function Speakers(props) {
   return (
     <div className="speaker_container">
       <NavHeader centerIt="Speakers" />
+      <Upper
+        UpperImg={speakerImg}
+        UpperTitle="new product"
+        UpperSubTitle="ZX9 SPEAKER"
+        UpperParagraph="Upgrade your sound system with the all new ZX9 active speaker. It’s a bookshelf speaker system that offers truly wireless connectivity -- creating new possibilities for more pleasing and practical audio setups."
+      />
     </div>
   );
 }
