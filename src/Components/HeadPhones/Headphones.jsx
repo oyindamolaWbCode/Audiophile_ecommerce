c1bdc68b8b3619858b7db0c70814f4b5c0c26e0b
oyindamolaWbCode <kushimo.oyindamola@gmail.com>
2024-08-13 16:26:21 +0100

    headphone page

--- src/Components/HeadPhones/Headphones.jsx
+++ src/Components/HeadPhones/Headphones.jsx
@@ -1,53 +1,56 @@
 import React from "react";
 import NavHeader from "../NavHeaders/NavHeader";
-import Upper from "../Uppercomponent/Upper";
-import headphoneImg from "../../assets/Images/image-xx99-mark-two.jpg";
+
 import "./Headphone.css";
 import HeadphoneFeatures from "../../Data/product.json";
+import Cards from "../Cards/Cards";
+import AudioGear from "../AudioGear/AudioGear";
 
 const Headphones = () => {
   return (
     <div className="headphones_container" style={{ backgroundColor: "white" }}>
       <NavHeader centerIt="Headphones" />
-      {/* <Upper
-        UpperImg={headphoneImg}
-        UpperTitle="new product"
-        UpperSubTitle="XX99 MARK II HEADPHONES"
-        UpperParagraph="The new XX99 Mark II headphones is the pinnacle of pristine audio. It redefines your premium headphone experience by reproducing the balanced depth and precision of studio-quality sound."
-      /> */}
-      {HeadphoneFeatures.headphones.map(
-        (headphone, index) => (
-          console.log(headphone.image.product),
-          (
-            <div
-              className={`index ${index === 1 ? "reverse-flex" : ""}`}
-              key={index}
-            >
-              <div className="upper-components">
-                <img
-                  src={headphone.image.product}
-                  // src={headphoneImg}
-                  alt="components"
-                  className="upperImgs"
-                />
+      <div className="headphone-content">
+        {HeadphoneFeatures.headphones.map(
+          (headphone, index) => (
+            console.log(headphone.image.product),
+            (
+              <div
+                className={`index ${index === 1 ? "reverse-flex" : ""}`}
+                key={index}
+              >
+                <div className="upper-components">
+                  <img
+                    src={headphone.image.product}
+                    // src={headphoneImg}
+                    alt="components"
+                    className="upperImgs"
+                  />
 
-                <div className="upper-details">
-                  <h3 className="new-product">NEW PRODUCT</h3>
-                  <h1 className="product-title">
-                    {headphone.shortName.toUpperCase()}
-                  </h1>
-                  <h1 className="product-title">
-                    {headphone.category.toUpperCase()}
-                  </h1>
-                  <p className="product-paragragh">{headphone.description}</p>
-                  <button className="product_view">See Product</button>
+                  <div className="upper-details">
+                    <h3 className="new-product">NEW PRODUCT</h3>
+                    <h1 className="product-title">
+                      {headphone.shortName.toUpperCase()}
+                    </h1>
+                    <h1 className="product-title">
+                      {headphone.category.toUpperCase()}
+                    </h1>
+                    <p className="product-paragragh">{headphone.description}</p>
+                    <button className="product_view">See Product</button>
+                  </div>
                 </div>
               </div>
-            </div>
+            )
           )
-        )
-        // console.log(headphone.image.product)
-      )}
+          // console.log(headphone.image.product)
+        )}
+        <div className="card-">
+          <Cards />
+        </div>
+        <div className="headphone-gear" style={{ marginBottom: "50px" }}>
+          <AudioGear />
+        </div>
+      </div>
     </div>
   );
 };
